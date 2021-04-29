c587cb05ddc7727edd7b5b9f4a62e1fd89c45612
Silva <bruno.a.silva@capgemini.com>
2021-04-29 11:32:34 -0300

    Teste funcionalidade camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {WebcamImage} from 'ngx-webcam';
+import {Subject, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'camera';
+  title = 'gfgangularwebcam';
+  
+  public webcamImage: WebcamImage = null;
+  private trigger: Subject<void> = new Subject<void>();
+  triggerSnapshot(): void {
+   this.trigger.next();
+  }
+  handleImage(webcamImage: WebcamImage): void {
+   console.info('Saved webcam image', webcamImage);
+   this.webcamImage = webcamImage;
+  }
+   
+  public get triggerObservable(): Observable<void> {
+   return this.trigger.asObservable();
+  }
 }
